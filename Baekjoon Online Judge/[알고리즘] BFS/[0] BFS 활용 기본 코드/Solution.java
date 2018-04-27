e49bc5adfc8f20de4915aab4843df6a738247b9e
Dong Ho Son <donghoson@users.noreply.github.com>
2018-04-28 01:06:44 +0900

    Update Solution.java

--- Baekjoon Online Judge/[알고리즘] BFS/[0] BFS 활용 기본 코드/Solution.java
+++ Baekjoon Online Judge/[알고리즘] BFS/[0] BFS 활용 기본 코드/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Main
 {
 	final static int N = 10001;
-	public static int[] Queue;					//BFS는 큐가 기본적으로 필요함.
+	public static int[] Queue;		//BFS는 큐가 기본적으로 필요함.
 	public static boolean isVisited[];
 	public static int hasRoad[][];
 	public static int front,end;
@@ -42,7 +42,8 @@ class Main
 			for(int i=0; i<N; i++)
 				isVisited[i]=false;
 			
-			//hasRoad[x][y] 을 초기화하는 처리가 필요. x 에서 y로 가는 길이 있다면 hasRoad[x][y] = 1로 표시한다.
+			//hasRoad[x][y] 을 초기화하는 처리가 필요. 
+			//x 에서 y로 가는 길이 있다면 hasRoad[x][y] = 1로 표시한다.
 			
 			//BFS호출
 			BFS();
@@ -50,18 +51,18 @@ class Main
 		}
 	}
 	
-    private static void BFS() {				//BFS는 DFS와 다르게 재귀로 하지 않아도 됨.
-        int start = 0;						//출발지 노드를 선택
-        push(start);						//출발지 노드를 큐에 push  #큐에 넣을때는 isVisited도 true로!
-        isVisited[start] = true;			//출발노드를 방문.
-        while(!isEmpty())					//큐가 빌때까지 반복한다.
+    private static void BFS() {			//BFS는 DFS와 다르게 재귀로 하지 않아도 됨.
+        int start = 0;				//출발지 노드를 선택
+        push(start);				//출발지 노드를 큐에 push  #큐에 넣을때는 isVisited도 true로!
+        isVisited[start] = true;		//출발노드를 방문.
+        while(!isEmpty())			//큐가 빌때까지 반복한다.
         {
-        	int current =pop();				//큐의 앞부분을 뽑아서
+        	int current =pop();		//큐의 앞부분을 뽑아서
             for(int next=0; next<N; next++)	//해당 노드와 연결되고, 아직 방문(탐색)하지 않은 노드들을 방문하고 큐에 push					
             {
             	if(hasRoad[current][next] == 1 && isVisited[next] == false) 
                 {
-                    isVisited[next] = true; //#큐에 넣을때는 isVisited도 true로!
+                    isVisited[next] = true; 	//#큐에 넣을때는 isVisited도 true로!
                     push(next);
                 }
             }
